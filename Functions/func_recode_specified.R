c5700a818e7619f8b3bbfb7ef7ed35fa095612fa
Lana Kern <lana.kern@student.uni-tuebingen.de>
2022-11-14 17:52:06 +0100

    Function for recoding specified and not specified values into 1 and 0 respectively.

---
+++ Functions/func_recode_specified.R
@@ -0,0 +1,23 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#### FUNCTION: RECODE (NOT) SPECIFIED VALUES ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+# generate function
+  ## input is data frame
+
+func_recode_specified <- function(data) {
+  
+  # identify variables with specified
+  vars_recode <- data %>%
+    select_if(~ any(. == "specified")) %>%
+    colnames()
+  
+  # replace: specified = 1, not specified = 0
+  data <- data %>% 
+    mutate_at(
+      all_of(vars_recode), 
+      list(~recode(., `specified` = 1, `not specified` = 0, .default = NaN))
+    )
+  
+  return(data)
+}
