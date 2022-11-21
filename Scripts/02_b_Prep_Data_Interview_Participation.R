5c42db91ac295be9c0be2c42971674102d93b9c0
Lana Kern <lana.kern@student.uni-tuebingen.de>
2022-11-21 20:32:47 +0100

    Adjust treatment periods

--- Scripts/02_b_Prep_Data_Interview_Participation.R
+++ Scripts/02_b_Prep_Data_Interview_Participation.R
@@ -58,6 +58,9 @@ library(tidyr)  # to work with missing values
 Sys.setlocale("LC_TIME", "German")
 
 
+cohort <- "test"
+
+
 
 #%%%%%%%%%%%%%%%%%#
 #### Load Data ####
@@ -157,135 +160,219 @@ data_cohort_profile <- data_cohort_profile %>%
 #### Generate Indices ####
 #%%%%%%%%%%%%%%%%%%%%%%%%#
 
+if (cohort == "test") {
+  
+  # End of treatment period #
+  #+++++++++++++++++++++++++#
+  
+  data_cohort_profile <- data_cohort_profile %>% 
+    mutate(wave_2 = ifelse(grepl("CAWI", wave), "CAWI", "CATI")) %>%
+    group_by(ID_t) %>%
+    mutate(
+      wave_2_lag = lag(wave_2),
+      CAWI_imp = ifelse(wave_2 == "CAWI" & wave_2 != wave_2_lag, 1, 0)
+      ) %>%
+    mutate(treatment_ends = ifelse(CAWI_imp == 1, cumsum(CAWI_imp), NA)) %>%
+    mutate(treatment_ends = treatment_ends - 1) %>%
+    replace_na(list(treatment_ends = 0))
+  
+  
+  # Start of treatment period #
+  #+++++++++++++++++++++++++++#
+  
+  data_cohort_profile <- data_cohort_profile %>%
+    mutate(treatment_ends = na_if(treatment_ends, 0)) %>%
+    mutate(treatment_starts = treatment_ends) %>% 
+    fill(treatment_starts, .direction = "up") %>%
+    mutate(treatment_starts = ifelse(!is.na(treatment_ends), treatment_ends + 1, treatment_starts)) %>%
+    ungroup()
+  
+  
+  # Time-invariant wave: always kept #
+  #++++++++++++++++++++++++++++++++++#
+  
+  # identify time-invariant control variables
+  # data_cohort_profile <- 
+  #   # create time invariant variable indicator for CATI and CAWI
+  #   rbind(
+  #     # select first CATI interview
+  #     data_cohort_profile %>%
+  #       arrange(ID_t, wave) %>%
+  #       group_by(ID_t) %>%
+  #       filter(grepl("CATI", wave)) %>% 
+  #       filter(row_number() == 1) %>%
+  #       mutate(controls_invariant = 1) %>%
+  #       select(ID_t, wave, controls_invariant),
+  #     # select first CAWI interview
+  #     data_cohort_profile %>%
+  #       arrange(ID_t, wave) %>%
+  #       group_by(ID_t) %>%
+  #       filter(grepl("CAWI", wave)) %>% 
+  #       filter(row_number() == 1) %>%
+  #       mutate(controls_invariant = 1) %>%
+  #       select(ID_t, wave, controls_invariant)
+  #   ) %>%
+  #   # merge this to full data frame (right join to keep everything in full df)
+  #   right_join(
+  #     data_cohort_profile, 
+  #     by = c("ID_t", "wave")
+  #   ) %>%
+  #   # sort data frame
+  #   arrange(ID_t, wave) %>%
+  #   ungroup()
+  
+  
+  # Handle CATI duplicates #
+  #+++++++++++++++++++++++#
+  
+  data_cohort_profile <- data_cohort_profile %>%
+    group_by(ID_t, treatment_starts, wave_2) %>% 
+    filter(case_when(
+      wave_2 == "CATI" ~ row_number() == n()
+    )) %>%
+    rbind(data_cohort_profile %>% filter(wave_2 == "CAWI")) %>% 
+    #rbind(data_cohort_profile %>% filter(wave_2 == "CAWI" | (wave_2 == "CATI" & controls_invariant == 1))) %>%
+    arrange(ID_t, wave, treatment_starts, treatment_ends) %>%
+    ungroup()
+  
 
-# Next, indices are generated which indicate the treatment period for each
-# respondent and from which wave, control, outcome and treatment variables
-# are taken.
-
-# time-invariant control variables are taken from the first two interviews
-# first CATI and first CAWI
-data_cohort_profile <- 
-  # create time invariant variable indicator for CATI and CAWI
-  rbind(
-    # select first CATI interview
+  # Handle CAWI duplicates #
+  #+++++++++++++++++++++++#
+  
+  data_cohort_profile <- 
     data_cohort_profile %>%
-      arrange(ID_t, wave) %>%
-      group_by(ID_t) %>%
-      filter(grepl("CATI", wave)) %>% 
-      filter(row_number() == 1) %>%
-      mutate(controls_invariant = 1),
-    # select first CAWI interview
-    data_cohort_profile %>%
-      arrange(ID_t, wave) %>%
-      group_by(ID_t) %>%
-      filter(grepl("CAWI", wave)) %>% 
-      filter(row_number() == 1) %>%
-      mutate(controls_invariant = 1)
-  ) %>%
-  # merge this to full data frame (right join to keep everything in full df)
-  right_join(
-    data_cohort_profile, 
-    by = c("ID_t", "wave", "interview_date", "competence_date", "competence_available_plausible_student")
+    ungroup() %>% group_by(ID_t) %>%
+    mutate(wave_2_lead = lead(wave_2)) %>%
+    mutate(treatment_starts = ifelse(
+      wave_2 == wave_2_lead, NA, treatment_starts
+    ))
+  
+} else {
+  # OLD VERSION
+  
+  # Next, indices are generated which indicate the treatment period for each
+  # respondent and from which wave, control, outcome and treatment variables
+  # are taken.
+  
+  # time-invariant control variables are taken from the first two interviews
+  # first CATI and first CAWI
+  data_cohort_profile <- 
+    # create time invariant variable indicator for CATI and CAWI
+    rbind(
+      # select first CATI interview
+      data_cohort_profile %>%
+        arrange(ID_t, wave) %>%
+        group_by(ID_t) %>%
+        filter(grepl("CATI", wave)) %>% 
+        filter(row_number() == 1) %>%
+        mutate(controls_invariant = 1),
+      # select first CAWI interview
+      data_cohort_profile %>%
+        arrange(ID_t, wave) %>%
+        group_by(ID_t) %>%
+        filter(grepl("CAWI", wave)) %>% 
+        filter(row_number() == 1) %>%
+        mutate(controls_invariant = 1)
     ) %>%
-  # sort data frame
-  arrange(ID_t, wave)
-
+    # merge this to full data frame (right join to keep everything in full df)
+    right_join(
+      data_cohort_profile, 
+      by = c("ID_t", "wave", "interview_date", "competence_date", "competence_available_plausible_student")
+    ) %>%
+    # sort data frame
+    arrange(ID_t, wave)
+  
   # for some individuals the control variables are taken from later waves
-data_cohort_profile[, c("wave", "controls_invariant")] %>% distinct() %>% na.omit()
-
-
-# count CATI and CAWI
-data_cohort_profile <- data_cohort_profile %>%
-  # generate cati and cawi variable
-  mutate(wave_2 = ifelse(grepl("CAWI", wave), "CAWI", "CATI")) %>%
-  # sort
-  arrange(ID_t, wave) %>%
-  # group by ID_t and cati/cawi and count (-> running number)
-  group_by(ID_t, wave_2) %>%
-  mutate(count = row_number()) %>%
-  # create a separate column for CATI and cAWI running number
-  pivot_wider(values_from = count, names_from = wave_2, names_prefix = "count_") %>%
-  # fill missing values up
-  fill(c(count_CATI), .direction = "down") %>%
-  # for CAWI replace missing values with zero (otherwise next computation makes troubles)
-  replace_na(list(count_CAWI = 0))
-
-
-
-## END OF TREATMENT PERIOD ##
-#+++++++++++++++++++++++++++#
-
-# treatment periods ends: for all CAWI
+  data_cohort_profile[, c("wave", "controls_invariant")] %>% distinct() %>% na.omit()
+  
+  
+  # count CATI and CAWI
+  data_cohort_profile <- data_cohort_profile %>%
+    # generate cati and cawi variable
+    mutate(wave_2 = ifelse(grepl("CAWI", wave), "CAWI", "CATI")) %>%
+    # sort
+    arrange(ID_t, wave) %>%
+    # group by ID_t and cati/cawi and count (-> running number)
+    group_by(ID_t, wave_2) %>%
+    mutate(count = row_number()) %>%
+    # create a separate column for CATI and cAWI running number
+    pivot_wider(values_from = count, names_from = wave_2, names_prefix = "count_") %>%
+    # fill missing values up
+    fill(c(count_CATI), .direction = "down") %>%
+    # for CAWI replace missing values with zero (otherwise next computation makes troubles)
+    replace_na(list(count_CAWI = 0))
+  
+  
+  
+  ## END OF TREATMENT PERIOD ##
+  #+++++++++++++++++++++++++++#
+  
+  # treatment periods ends: for all CAWI
   ## first treatment period ends when count_CAWI = 2 -> second condition is
   ## that at least one CATI information is available at this time
   ## second treatment period ends when count_CAWI = 3 -> second condition is
   ## that student also participated in second CATI period.
-i_start <- 1
-i_stop <- max(data_cohort_profile$count_CATI) - 1
-data_cohort_profile$treatment_ends <- NA
-for (i in i_start:i_stop) {
-  data_cohort_profile[data_cohort_profile$count_CAWI >= (i + 1) & # before: "==" not working
-                      data_cohort_profile$count_CATI >= i, 
-                      "treatment_ends"] <- i
-  i <- i + 1
-}
+  i_start <- 1
+  i_stop <- max(data_cohort_profile$count_CATI) - 1
+  data_cohort_profile$treatment_ends <- NA
+  for (i in i_start:i_stop) {
+    data_cohort_profile[data_cohort_profile$count_CAWI >= (i + 1) & # before: "==" not working
+                          data_cohort_profile$count_CATI >= i,
+                        "treatment_ends"] <- i
+    i <- i + 1
+  }
   ## if two treatment_ends with same number keep first one and set other NA
   ## show example
-#test <- data_cohort_profile %>% subset(ID_t %in% c(7001969, 7016717, 7004031, 7003396, 7001968))
-data_cohort_profile <- data_cohort_profile %>%
-  group_by(ID_t, treatment_ends) %>%
-  mutate(treatment_ends = ifelse(duplicated(treatment_ends), NA, treatment_ends)) 
+  #test <- data_cohort_profile %>% subset(ID_t %in% c(7001969, 7016717, 7004031, 7003396, 7001968))
+  data_cohort_profile <- data_cohort_profile %>%
+    group_by(ID_t, treatment_ends) %>%
+    mutate(treatment_ends = ifelse(duplicated(treatment_ends), NA, treatment_ends))
   ## adjust enumerator for treatment_ends variable
-  ## for some respondents where two CAWI surveys are conducted in a row 
+  ## for some respondents where two CAWI surveys are conducted in a row
   ## (yields a gap of 1 in the treatment_ends variable)
-data_cohort_profile <- 
-  rbind(
-    data_cohort_profile %>% filter(is.na(treatment_ends)),
-    data_cohort_profile %>%
-      group_by(ID_t) %>%
-      filter(!is.na(treatment_ends)) %>%
-      mutate(treatment_ends = row_number())
-  ) %>%
-  arrange(ID_t, wave, interview_date)
-
-
-
-## START OF TREATMENT PERIOD ##
-#+++++++++++++++++++++++++++++#
-
-# treatment period starts: 
+  data_cohort_profile <-
+    rbind(
+      data_cohort_profile %>% filter(is.na(treatment_ends)),
+      data_cohort_profile %>%
+        group_by(ID_t) %>%
+        filter(!is.na(treatment_ends)) %>%
+        mutate(treatment_ends = row_number())
+    ) %>%
+    arrange(ID_t, wave, interview_date)
+  
+  
+  
+  ## START OF TREATMENT PERIOD ##
+  #+++++++++++++++++++++++++++++#
+  
+  # treatment period starts: 
   ## first one starts in first row per group
   ## from the second one, they start when the outer finish
-data_cohort_profile <- data_cohort_profile %>%
-  arrange(ID_t, wave) %>%
-  group_by(ID_t) %>%
-  # first treatment start
-  mutate(treatment_starts = ifelse(row_number() == 1, 1, NA)) %>%
-  # further treatment start
-  mutate(treatment_starts = ifelse(!is.na(treatment_ends), 
-                                   treatment_ends + 1, treatment_starts)) %>%
-  # fill up missings to ensure that control variables can be taken from
-  # respective wave according to treatment_starts variable
-  fill(treatment_starts, .direction = "down")
+  data_cohort_profile <- data_cohort_profile %>%
+    arrange(ID_t, wave) %>%
+    group_by(ID_t) %>%
+    # first treatment start
+    mutate(treatment_starts = ifelse(row_number() == 1, 1, NA)) %>%
+    # further treatment start
+    mutate(treatment_starts = ifelse(!is.na(treatment_ends), 
+                                     treatment_ends + 1, treatment_starts)) %>%
+    # fill up missings to ensure that control variables can be taken from
+    # respective wave according to treatment_starts variable
+    fill(treatment_starts, .direction = "down")
   ## replace last treatment start with NA when no further treatment ends
   # extract maximum treatment end for each ID
-sub <-  data_cohort_profile %>% 
-  group_by(ID_t) %>% 
-  summarise(max_tr_ends = max(treatment_ends, na.rm = TRUE))
+  sub <-  data_cohort_profile %>% 
+    group_by(ID_t) %>% 
+    summarise(max_tr_ends = max(treatment_ends, na.rm = TRUE))
   # merge variable with maximum to data
-data_cohort_profile <- left_join(data_cohort_profile, sub, by = "ID_t")
+  data_cohort_profile <- left_join(data_cohort_profile, sub, by = "ID_t")
   # replace treatment_starts with NA if treatment_starts is larger than maximum
   # number of treatment ends as this implies that no new treatment period starts
   # because no ending observations are given
-data_cohort_profile <- data_cohort_profile %>%
-  mutate(treatment_starts = ifelse(treatment_starts > max_tr_ends, NA, treatment_starts))
-
-
-
-# show example
-# data_cohort_profile %>% 
-#   subset(ID_t %in% c(7001969, 7016717, 7004031, 7003396, 7001968))
-
+  data_cohort_profile <- data_cohort_profile %>%
+    mutate(treatment_starts = ifelse(treatment_starts > max_tr_ends, NA, treatment_starts))
+  
+}
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%#
@@ -328,17 +415,36 @@ data_cohort_profile <- data_cohort_profile %>%
 length(unique(data_cohort_profile$ID_t))
 
 # following surveys
-  ## create wave variable
-data_cohort_profile <- data_cohort_profile %>%
-  mutate(wave_2 = ifelse(grepl("CAWI", wave), "CAWI", "CATI"))
+  ## create wave variable if not "test" run
+if (cohort == "test") {
+  data_cohort_profile <- data_cohort_profile
+} else {
+  data_cohort_profile <- data_cohort_profile %>%
+    mutate(wave_2 = ifelse(grepl("CAWI", wave), "CAWI", "CATI"))
   ## if two CAWI surveys appear sequentially, that is for instance CAWI12 CAWI13 CATI13 CAWI14
   ## the second CAWI row is kept so that CAWI13 CATI13 CAWI14
   ## individual example: 7003396
   ## if two CATI survey appear sequentially, the same is done 
   ## individual example: 7001969
-data_cohort_profile <- data_cohort_profile %>%
-  group_by(ID_t, treatment_starts, wave_2) %>% 
-  filter(row_number() == n())
+  data_cohort_profile <- data_cohort_profile %>%
+    group_by(ID_t, treatment_starts, wave_2) %>% 
+    filter(row_number() == n())
+}
+
+
+# # there are mistakes in generating treatment periods: FIX THIS 
+# data_cohort_profile %>% subset(ID_t == 7002011) # example (see period 3)
+# data_cohort_profile %>% subset(ID_t == 7002044) # example (see period 3)
+# 
+#   ## keep only rows with duplicates in ID_t and treatment_starts
+# data_cohort_profile %>%
+#   group_by(ID_t, treatment_starts) %>%
+#   filter(n() == 1 & !is.na(treatment_starts))
+# 
+# data_cohort_profile[(!(duplicated(data_cohort_profile$treatment_starts) | 
+#                          duplicated(data_cohort_profile$treatment_starts, fromLast = TRUE))) & 
+#                       !is.na(data_cohort_profile$treatment_starts), "treatment_starts"] <- NA
+
 
 
 # number of individuals left
@@ -357,14 +463,22 @@ print(
 data_cohort_profile <- data_cohort_profile %>%
   ungroup() %>%
   select(ID_t, wave, interview_date, competence_date, competence_available_plausible_student,  
-         controls_invariant, treatment_ends, treatment_starts)
+         treatment_ends, treatment_starts)
+
+# check computations based on examples
+check_1 <- data_cohort_profile %>%
+  subset(ID_t %in% c(7001969, 7016717, 7004031, 7003396, 7001968)) %>%
+  select(ID_t, wave, treatment_starts, treatment_ends)
+
+check_2 <- data_cohort_profile %>% 
+  subset(ID_t %in% c(7001988, 7002011, 7002025, 7002058)) %>%
+  select(ID_t, wave, treatment_starts, treatment_ends)
 
 # number of respondents, number of rows and columns
 print(paste("Number of respondents:", length(unique(data_cohort_profile$ID_t))))
 print(paste("Number of rows", nrow(data_cohort_profile)))
 print(paste("Number of columns", ncol(data_cohort_profile)))
   
-
 # save data
 saveRDS(data_cohort_profile, "Data/Prep_2/prep_2_cohort_profile.rds")
   
