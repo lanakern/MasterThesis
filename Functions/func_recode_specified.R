8945f1171681a633dcb75742c47b9dcc6a43bd8c
Lana Kern <lana.kern@student.uni-tuebingen.de>
2022-11-23 17:05:57 +0100

    Convert NaN in NA

--- Functions/func_recode_specified.R
+++ Functions/func_recode_specified.R
@@ -16,8 +16,13 @@ func_recode_specified <- function(data) {
   data <- data %>% 
     mutate_at(
       all_of(vars_recode), 
-      list(~recode(., `specified` = 1, `not specified` = 0, .default = NaN))
-    )
+      list(
+        ~recode(., `specified` = 1, `not specified` = 0, 
+                .default = NaN) # NA does not work
+           ) 
+    ) %>%
+    # replace NaN with NA
+    mutate_all(~ifelse(is.nan(.), NA, .))
   
   return(data)
 }
