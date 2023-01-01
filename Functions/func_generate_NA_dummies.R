b711dbefa50e0ad4286f24bdbeb076b00135c600
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-01-01 12:18:50 +0100

    Generate function for NA dummy generation if variable has missings

---
+++ Functions/func_generate_NA_dummies.R
@@ -0,0 +1,18 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#### FUNCTION: GENERATE NA DUMMIES ##
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+
+# this function generated NA dummies:
+# input data: data frame ("data") containing variable ("variable_NA_dummy")
+# which should be converted to a NA dummy
+# output: data frame with NA dummy
+
+func_generate_NA_dummies <- function(data, variable_NA_dummy) {
+  varname_NA <- paste0(variable_NA_dummy, "_NA")
+  data <- data %>%
+    mutate(
+      {{varname_NA}} := case_when(is.na(!!!rlang::syms(variable_NA_dummy)) ~ 1, TRUE ~ 0)
+    )
+  return(data)
+}
