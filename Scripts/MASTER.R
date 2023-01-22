34ee74ed05e0f79b8d347caf882a354ae459117b
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-01-22 15:41:20 +0100

    Define variables for baseline model

--- Scripts/MASTER.R
+++ Scripts/MASTER.R
@@ -70,6 +70,7 @@ Sys.setlocale("LC_TIME", "German")
 #           "prep_sel_num", "aggr_vars", "cronbach_a")
 #   ))
 
+
 #### DEFINE INPUTS ####
 #%%%%%%%%%%%%%%%%%%%%%#
 
@@ -104,6 +105,43 @@ df_inputs <- data.frame(
 df_inputs <- df_inputs %>% expand(cohort_prep, treatment_repl, treatment_def) %>% na.omit()
 
 
+# define variables for baseline model
+vars_baseline <- 'select(
+group, starts_with("outcome"), starts_with("treatment"),
+
+interview_start_year_num, interview_end_year_num,
+
+gender_male, age, birth_year_num, birth_country_ger, birth_ger_eastwest_west,
+migration, childhood_biological_parents, religion_christian,
+bilingual, kindergarden,
+
+starts_with("health"),
+
+educ_years_total, educ_years_current_uni, starts_with("educ_school_degree"),
+starts_with("educ_school_grade"), educ_school_rep_grade,
+
+starts_with("motivation_degree"), starts_with("uni_degree_importance_well"), starts_with("uni_major"),
+starts_with("uni_fear"), starts_with("uni_quali"), starts_with("uni_time"), uni_type_uni_general, 
+
+starts_with("interest_art_muesum"), interest_math, interest_music_play, interest_politics, interest_reading_leisure, 
+
+starts_with("comp_"), 
+emp_current, emp_current_act_work_hours, 
+
+starts_with("mother_degree_highest"), starts_with("father_degree_highest"), mother_emp_bef_15y, father_emp_bef_15y, 
+starts_with("mother_emp_prof_"), starts_with("father_emp_prof_"), starts_with("parents"),
+starts_with("friends_study_share"), 
+
+starts_with("personality"), starts_with("bigfive"), 
+
+starts_with("satisfaction_life"), starts_with("satisfaction_study"), starts_with("social_integr"), 
+
+sibling_total, partner_current, child, living_alone, living_hh_size, starts_with("place_residence")
+
+)'
+
+
+
 #### LOAD ALL FUNCTIONS ####
 #%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
@@ -115,7 +153,7 @@ for (func_load in load_function) {
 
 keep_after_file_run <- 'rm(list = setdiff(ls(), c("cohort_prep", "treatment_repl", 
 "treatment_def", "df_inputs", "prep_sel_num", "aggr_vars", "cronbach_a", "keep_after_file_run",
-ls()[str_starts(ls(), "func_")])))'
+"vars_baseline", ls()[str_starts(ls(), "func_")])))'
 
 
 #### RUN DATA PREPARATION ####
@@ -192,10 +230,23 @@ for (prep_sel_num in 1:nrow(df_inputs)) {
 df_excel_save_hist <- read.xlsx("Data/SAMPLE_REDUCTION_STEPS.xlsx", sheetName = "Sheet1")
 df_excel_save_hist
 
-
-# Prepare control variables
 # now set language for dates and times to English
 Sys.setlocale("LC_TIME", "English")
+
+# perform further steps withput sample selection reduction
+for (prep_sel_num in 1:nrow(df_inputs)) {
+  
+  df_inputs_sel <- df_inputs[prep_sel_num, ]
+  cohort_prep <- df_inputs_sel$cohort_prep
+  treatment_repl <- df_inputs_sel$treatment_repl
+  treatment_def <- df_inputs_sel$treatment_def
+  
+  # Prepare control variables
+  eval(parse(text = keep_after_file_run))
+  source("Scripts/07_Create_Control_Variables.R") 
+  
+}
+
 
 # Plausibility analysis
 
