81c79ba8fd78aa5dbf7bfe98756172d87587d49d
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-01-24 13:45:34 +0100

    Without cross-validation

--- Functions/func_dml_theta_score.R
+++ Functions/func_dml_theta_score.R
@@ -22,64 +22,49 @@
 # -> score_ATTE_all: values for the score function of the ATTE
 #++++
 
-func_dml_theta_score <- function(data, outcome, treatment, data_pred, K_folds) {
+func_dml_theta_score <- function(data_pred, data_test, outcome, treatment) {
+
   
-  # create empty vectors to store results
-    ## to store treatment effect estimates
-  theta_ATE_all <- c()
-  theta_ATTE_all <- c()
-    ## to store values of the score function
-  score_ATE_all <- c()
-  score_ATTE_all <- c()
+  # extract outcome, treatment, etc.
+  Y <- data_test[, outcome]  # outcome
+  D <- data_test[, treatment] # treatment
+  n <- nrow(data_test) # number of observations in test data
+  m <- sum(data_test %>% pull(all_of(treatment))) / n # E(D)
+  m_pred <- data_pred %>% pull(m)
+  g0_pred <- data_pred %>% pull(g0)
+  g1_pred <- data_pred %>% pull(g1)
   
-  # iterate over folds
-  for (fold_sel in 1:nrow(K_folds)) {
-    
-    # extracting test data
-    indices_fold_sel <- K_folds$splits[[fold_sel]]$in_id
-    data_test <- data[-indices_fold_sel, ]
-    
-    # extract outcome, treatment, etc.
-    Y <- data_test[, outcome] %>% pull() # outcome
-    D <- data_test[, treatment] %>% pull() # treatment
-    n <- nrow(data_test) # number of observations in test data
-    m <- sum(data_test %>% dplyr::select(all_of(treatment)) %>% pull()) / n # E(D)
-    m_pred <- data_pred %>% filter(fold == fold_sel) %>% pull(m)
-    g0_pred <- data_pred %>% filter(fold == fold_sel) %>% pull(g0)
-    g1_pred <- data_pred %>% filter(fold == fold_sel) %>% pull(g1)
-    
-    
-    ## ATE ##
-    #-------#
-    
-    # calculate theta
-    score_a_ATE <- rep(-1, nrow(data_test))
-    pseudo_0_ATE <- g0_pred + (1 - D) * (Y - g0_pred) / (1 - m_pred)
-    pseudo_1_ATE <- g1_pred + D * (Y - g1_pred) / m_pred
-    score_b_ATE <- pseudo_1_ATE - pseudo_0_ATE
-    theta_ATE <- -sum(score_b_ATE) / sum(score_a_ATE)
-    theta_ATE_all <- c(theta_ATE_all, theta_ATE)
-    
-    # calculate score
-    score_ATE <- theta_ATE * score_a_ATE + score_b_ATE
-    score_ATE_all <- c(score_ATE_all, score_ATE)
-    
-    
-    ## ATTE ##
-    #--------#
-    
-    # calculate theta
-    score_a_ATTE <- -D / m 
-    score_b_ATTE <- D * (Y - g0_pred) / m - 
-      m_pred * (1 - D) * (Y - g0_pred) * (m*(1 - m_pred))
-    theta_ATTE <- -sum(score_b_ATTE) / sum(score_a_ATTE)
-    theta_ATTE_all <- c(theta_ATTE_all, theta_ATTE)
-    
-    # calculate score
-    score_ATTE <- theta_ATTE * score_a_ATTE + score_b_ATTE
-    score_ATTE_all <- c(score_ATTE_all, score_ATTE)
-  }
   
-  return(list("score_ATE_all" = score_ATE_all, "theta_ATE_all" = theta_ATE_all, 
-              "score_ATTE_all" = score_ATTE_all, "theta_ATTE_all" = theta_ATTE_all))
+  ## ATE ##
+  #-------#
+  
+  # calculate theta
+  score_a_ATE <- rep(-1, nrow(data_test))
+  pseudo_0_ATE <- g0_pred + (1 - D) * (Y - g0_pred) / (1 - m_pred)
+  pseudo_1_ATE <- g1_pred + D * (Y - g1_pred) / m_pred
+  score_b_ATE <- pseudo_1_ATE - pseudo_0_ATE
+  theta_ATE <- -sum(score_b_ATE) / sum(score_a_ATE)
+
+  
+  # calculate score
+  score_ATE <- theta_ATE * score_a_ATE + score_b_ATE
+
+  
+  
+  ## ATTE ##
+  #--------#
+  
+  # calculate theta
+  score_a_ATTE <- -D / m 
+  score_b_ATTE <- D * (Y - g0_pred) / m - 
+    m_pred * (1 - D) * (Y - g0_pred) * (m*(1 - m_pred))
+  theta_ATTE <- -sum(score_b_ATTE) / sum(score_a_ATTE)
+
+  
+  # calculate score
+  score_ATTE <- theta_ATTE * score_a_ATTE + score_b_ATTE
+
+  
+  return(list("score_ATE" = score_ATE, "theta_ATE_all" = theta_ATE, 
+              "score_ATTE" = score_ATTE, "theta_ATTE_all" = theta_ATTE))
 }
