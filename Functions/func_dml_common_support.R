28b8ae7652ed7edd6987eda832a812d20fdeadb9
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-01-25 17:18:24 +0100

    Function for plot for checking common support and visualizing trimming thresholds

--- Functions/func_dml_common_support.R
+++ Functions/func_dml_common_support.R
@@ -1,21 +1,28 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#### FUNCTION FOR CHECKING COMMON SUPPORT ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
-func_dml_common_support <- function(data_pred, type) {
+
+func_dml_common_support <- function(data_pred, min_trimming, max_trimming) {
   
-  if (type == "side-by-side") {
-    # https://sejdemyr.github.io/r-tutorials/statistics/tutorial8.html
-    data_pred %>%
-      mutate(treatment_label = ifelse(treatment == 1, "Sport Participatiopn", "No Sport Participation")) %>%
-      ggplot(aes(x = m)) +
-      geom_histogram(color = "white", binwidth = 0.01) +
-      facet_wrap(~treatment_label) +
-      xlab("Probability of participating in sports") +
-      theme_bw()
-  } else if (type == "one-plot") {
-    data_pred %>%
-      mutate(treatment_label = ifelse(treatment == 1, "Sport Participatiopn", "No Sport Participation")) %>%
-      ggplot(aes(x = m, color = treatment_label)) +
-      geom_histogram(binwidth = 0.01)
-  }
+  min_trimming <- min(min_trimming)
+  max_trimming <- min(max_trimming)
+  
+  data_pred %>%
+    mutate(treatment_label = ifelse(treatment == 1, "Sport Participatiopn", "No Sport Participation")) %>%
+    ggplot(aes(x = m, fill = treatment_label)) +
+    geom_histogram(binwidth = 0.01) +
+    scale_fill_manual(values = c("grey88", "grey38")) +
+    geom_vline(xintercept = min_trimming, linetype = "longdash", 
+               color = "black", size = 0.5) +
+    geom_vline(xintercept = max_trimming, linetype = "longdash", 
+               color = "black", size = 0.5) + 
+    xlab("Probability of participating in sports") + 
+    ylab("Number of observations") + 
+    ggtitle("Propensity Score Overlap") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    guides(fill = guide_legend(title = "Treatment Group"))
 }
 
 
