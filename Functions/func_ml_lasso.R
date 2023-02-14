c71d30bcdf3d0bda9f92a2606944278f326d1b8e
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-02-14 16:19:02 +0100

    replaced treatment_sport by treatment user input

--- Functions/func_ml_lasso.R
+++ Functions/func_ml_lasso.R
@@ -31,8 +31,8 @@ func_ml_lasso <- function(data_train, data_test, outcome, treatment, group, K, l
   data_test <- data_test %>% mutate({{treatment}} := as.factor(!!sym(treatment))) 
   
   # separate training data for g0 and g1 prediction
-  data_train_g1 <- data_train %>% filter(treatment_sport == 1)
-  data_train_g0 <- data_train %>% filter(treatment_sport == 0)
+  data_train_g1 <- data_train %>% filter(!!sym(treatment) == 1)
+  data_train_g0 <- data_train %>% filter(!!sym(treatment) == 0)
   
   # specify the model
     ## logistic regression with lasso for treatment; linear regression with lasso for outcome
