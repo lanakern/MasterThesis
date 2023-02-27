6c2fef20d1ac59c113e2dfecc09a3ebdf8f65256
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-02-27 13:37:40 +0100

    Add DML results analysis for multivalued treatment setting

--- Scripts/12_e_Analysis_DML_Results.R
+++ Scripts/12_e_Analysis_DML_Results.R
@@ -14,6 +14,7 @@
 #### LOAD DATA ####
 #%%%%%%%%%%%%%%%%%#
 
+#+++++++++++++++++++++#
 ## MAIN LOAD RESULTS ##
 #+++++++++++++++++++++#
 
@@ -24,6 +25,7 @@ df_dml_main_multi <-
   read.xlsx("Output/DML/DML_MULTI_ESTIMATION_RESULTS.xlsx", sheetName = "Sheet1")
 
 
+#+++++++++++++++++++++++++++++#
 ## LOAD RESULTS ACROSS FOLDS ##
 #+++++++++++++++++++++++++++++#
 
@@ -38,7 +40,20 @@ load_results_main <- paste(
   main_treatment_def, main_treatment_repl, main_extra_act_save, sep = "_"
 )
 
+## APE ##
+#-------#
+
+# the APE is identical across the MICE data sets and algorithms
+
+# BINARY TREATMENT SETTING #
+
+# MULTIVALUED TREATMENT SETTING #
+df_dml_load_multi <- readRDS(paste0("Output/DML/multi_", "xgboost", "_", load_results_main, ".rds"))
+df_dml_ape_multi <- df_dml_load_multi[[1]]$ape 
+
+
 ## DETAIL ##
+#----------#
 
 # BINARY TREATMENT SETTING #
 df_dml_detail_all_binary <- data.frame()
@@ -58,10 +73,24 @@ for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
 
 
 # MULTIVALUED TREATMENT SETTING #
-
+df_dml_detail_all_multi <- data.frame()
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_multi <- readRDS(paste0("Output/DML/multi_", model_algo_sel, "_", load_results_main, ".rds"))
+  # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
+  
+  df_dml_detail_sub_all <- data.frame()
+  for (i in 1:5) {
+    df_dml_detail_sub <- df_dml_load_multi[[i]]$detail %>% filter(Type %in% c("ATE", "ATTE")) %>% mutate(MICE = i)
+    df_dml_detail_sub_all <- rbind(df_dml_detail_sub_all, df_dml_detail_sub)
+  }
+  
+  df_dml_detail_all_multi <- rbind(df_dml_detail_all_multi, df_dml_detail_sub_all)
+  
+}
 
 
 ## ERROR METRICS ##
+#-----------------#
 
 # BINARY TREATMENT SETTING #
 df_dml_error_all_binary <- data.frame()
@@ -81,16 +110,30 @@ for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
 
 
 # MULTIVALUED TREATMENT SETTING #
+df_dml_error_all_multi <- data.frame()
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_multi <- readRDS(paste0("Output/DML/multi_", model_algo_sel, "_", load_results_main, ".rds"))
+  # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
+  
+  df_dml_error_sub_all <- data.frame()
+  for (i in 1:5) {
+    df_dml_error_sub <- df_dml_load_multi[[i]]$error %>% mutate(MICE = i, modelalgo = model_algo_sel)
+    df_dml_error_sub_all <- rbind(df_dml_error_sub_all, df_dml_error_sub)
+  }
+  
+  df_dml_error_all_multi <- rbind(df_dml_error_all_multi, df_dml_error_sub_all)
+}
 
 
 ## PREDICTIONS (FOR COMMON SUPPORT) ##
+#------------------------------------#
 
-# FILE PATH ANPASSEN UND ALGOS
+# GEHT NOCH NICHT BEI BINARY
 
 # BINARY TREATMENT SETTING #
 df_dml_pred_all_binary <- data.frame()
-for (model_algo_sel in c("lasso")) {
-  df_dml_load_binary <- readRDS(paste0("Output/DML/TEST_binary_", model_algo_sel, "_", load_results_main, ".rds"))
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_binary <- readRDS(paste0("Output/DML/binary_", model_algo_sel, "_", load_results_main, ".rds"))
   # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
   
   df_dml_pred_sub_all <- data.frame()
@@ -100,18 +143,32 @@ for (model_algo_sel in c("lasso")) {
   }
   
   df_dml_pred_all_binary <- rbind(df_dml_pred_all_binary, df_dml_pred_sub_all)
-  
 }
 
 
 # MULTIVALUED TREATMENT SETTING #
+df_dml_pred_all_multi <- data.frame()
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_multi <- readRDS(paste0("Output/DML/multi_", model_algo_sel, "_", load_results_main, ".rds"))
+  # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
+  
+  df_dml_pred_sub_all <- data.frame()
+  for (i in 1:5) {
+    df_dml_pred_sub <- df_dml_load_multi[[i]]$pred %>% mutate(MICE = i, modelalgo = model_algo_sel)
+    df_dml_pred_sub_all <- rbind(df_dml_pred_sub_all, df_dml_pred_sub)
+  }
+  
+  df_dml_pred_all_multi <- rbind(df_dml_pred_all_multi, df_dml_pred_sub_all)
+}
 
 
 ## TRIMMING ##
+#------------#
 
+# BINARY TREATMENT SETTING #
 df_dml_trimming_all_binary <- data.frame()
-for (model_algo_sel in c("lasso")) {
-  df_dml_load_binary <- readRDS(paste0("Output/DML/TEST_binary_", model_algo_sel, "_", load_results_main, ".rds"))
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_binary <- readRDS(paste0("Output/DML/binary_", model_algo_sel, "_", load_results_main, ".rds"))
   # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
   
   df_dml_trimming_sub_all <- data.frame()
@@ -125,6 +182,25 @@ for (model_algo_sel in c("lasso")) {
 }
 
 
+# MULTIVALUED TREATMENT SETTING #
+df_dml_trimming_all_multi <- data.frame()
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_load_multi <- readRDS(paste0("Output/DML/multi_", model_algo_sel, "_", load_results_main, ".rds"))
+  # assign(paste0("dml_result_", model_algo_sel), df_dml_load)
+  
+  df_dml_trimming_sub_all <- data.frame()
+  for (i in 1:5) {
+    df_dml_trimming_sub <- df_dml_load_multi[[i]]$trimming %>% mutate(MICE = i, modelalgo = model_algo_sel)
+    df_dml_trimming_sub_all <- rbind(df_dml_trimming_sub_all, df_dml_trimming_sub)
+  }
+  
+  df_dml_trimming_all_multi <- rbind(df_dml_trimming_all_multi, df_dml_trimming_sub_all)
+  
+}
+
+
+
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%#
 #### TREATMENT EFFECTS ####
 #%%%%%%%%%%%%%%%%%%%%%%%%%#
@@ -135,10 +211,16 @@ df_dml_main_binary %>% select(
   starts_with("model"), Treatment, Type, starts_with("theta"),
   starts_with("se"), starts_with("tvalue"), starts_with("pvalue"), 
   starts_with("CI")
-)
+) %>% filter(Type %in% c("ATE", "ATTE"))
 
 
 ## Multivalued Treatment Setting ##
+df_dml_main_multi %>% select(
+  cohort_prep, treatment_repl, treatment_def, extra_act,
+  starts_with("model"), Treatment, Type, starts_with("theta"),
+  starts_with("se"), starts_with("tvalue"), starts_with("pvalue"), 
+  starts_with("CI")
+) %>% filter(Type %in% c("ATE", "ATTE"))
 
 
 ## FUNCTION RESULTS ##
@@ -176,6 +258,11 @@ readRDS("Output/DML/FUNCTION_DOUBLEML.rds")
 #   select(!starts_with("num")) %>% select(matches("_[g][1-9]"), ends_with("_m")) %>% distinct()
 
 
+#%%%%%%%%%%%%%%%%%#
+#### APO & APE ####
+#%%%%%%%%%%%%%%%%%#
+
+
 #%%%%%%%%%%%%%%%%%%%%%#
 #### ERROR METRICS ####
 #%%%%%%%%%%%%%%%%%%%%%#
@@ -189,7 +276,11 @@ df_dml_main_binary %>% select(
 
 
 ## Multivalued Treatment Setting ##
-
+df_dml_main_multi %>% select(
+  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), 
+  starts_with("ACC"), starts_with("KAPPA"), starts_with("BACC"), starts_with("AUC"),
+  starts_with("MAE"), starts_with("MAPE"), starts_with("MSE"), starts_with("RMSE")
+) %>% distinct()
 
 
 #%%%%%%%%%%%%%%%%#
@@ -198,23 +289,32 @@ df_dml_main_binary %>% select(
 
 ## Binary Treatment Setting ##
 df_dml_main_binary %>% select(
-  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), 
-  n_treats_min
+  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), n_treats_min
 ) %>% distinct()
 
 
 ## Multivalued Treatment Setting ##
-
+df_dml_main_multi %>% select(
+  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), n_treats_min
+) %>% distinct()
 
 
 #%%%%%%%%%%%%%%%%%%#
 #### PREDICTORS ####
 #%%%%%%%%%%%%%%%%%%#
 
+
+## Binary Treatment Setting ##
 df_dml_main_binary %>% select(
-  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), 
-  starts_with("num_predictors")
+  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), starts_with("num_predictors")
 ) %>% distinct()
+
+
+## Multivalued Treatment Setting ##
+df_dml_main_multi %>% select(
+  cohort_prep, treatment_repl, treatment_def, extra_act, starts_with("model"), starts_with("num_predictors")
+) %>% distinct()
+
 
 
 #%%%%%%%%%%%%%%%%%%%%%%#
@@ -223,14 +323,36 @@ df_dml_main_binary %>% select(
 
 ## Binary Treatment Setting ##
 
+# common support plot across all mice data frames and all repetitions
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_pred_all_binary_sub <- df_dml_pred_all_binary %>% 
+    filter(modelalgo %in% model_algo_sel) %>%
+    left_join(df_dml_trimming_all_binary, by = "modelalgo")
+
+  binary_plot_common_support <- func_dml_common_support(
+    "binary", df_dml_pred_all_binary_sub, df_dml_pred_all_binary_sub$min_trimming, 
+    df_dml_pred_all_binary_sub$max_trimming)
+  ggsave(paste0("Output/DML/binary_all_dml_plot_common_support_binary_", model_algo_sel, ".png"), 
+         binary_plot_common_support)
+}
+
+
+## Multivalued Treatment Setting ##
 
 # common support plot across all mice data frames and all repetitions
-binary_plot_common_support <- 
-  func_dml_common_support(treatment_setting, df_dml_pred_all_binary, 
-                          df_dml_trimming_all_binary$min_trimming, 
-                          df_dml_trimming_all_binary$max_trimming)
-ggsave(paste0("Output/DML/binary_all_dml_plot_common_support_", treatment_setting, "_", mlalgo, ".png"), 
-       binary_plot_common_support)
+for (model_algo_sel in c("lasso", "postlasso", "randomforests", "xgboost")) {
+  df_dml_pred_all_multi_sub <- df_dml_pred_all_multi %>% 
+    filter(modelalgo %in% model_algo_sel) %>%
+    left_join(
+      df_dml_trimming_all_multi, by = "modelalgo"
+    )
+  multi_plot_common_support <- func_dml_common_support(
+    "multi", df_dml_pred_all_multi_sub, df_dml_pred_all_multi_sub$min_trimming, 
+    df_dml_pred_all_multi_sub$max_trimming)
+  ggsave(paste0("Output/DML/binary_all_dml_plot_common_support_multi_", model_algo_sel, ".png"), 
+         multi_plot_common_support)
+}
+
 
 
 #%%%%%%%%%%%%%%%#
@@ -274,6 +396,51 @@ ggsave("Output/DML/binary_plot_dml_boxplot.png", dml_boxplot_binary)
 ## MULTIVALUED TREATMENT SETTING ##
 #+++++++++++++++++++++++++++++++++#
 
+df_dml_results_boxplot_multi <- df_dml_detail_all_multi %>%
+  mutate(
+    ML_algo = case_when(
+      ML_algo == "lasso" ~ "Lasso", ML_algo == "postlasso" ~ "Post-Lasso",
+      ML_algo == "randomforests" ~ "Random Forests",
+      ML_algo == "xgboost" ~ "XGBoost"
+    )
+  )
+
+# ATE
+df_dml_results_ate_boxplot_multi <- df_dml_results_boxplot_multi %>% filter(Type == "ATE")
+dml_ate_boxplot_multi <- 
+  ggplot(df_dml_results_ate_boxplot_multi, aes(x = ML_algo, y = Treatment_Effect)) +
+  geom_boxplot(fill = "grey") +
+  #xlab("\nMachine Learning Algorithms\n") +
+  xlab("") + ylab("\nATE\n") +
+  facet_wrap(~ Treatment) + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 28), # size of x-axis tick labels
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1), # rotate x-axis ticks
+        axis.title = element_text(size = 30, face = "bold"), # size of x-axis labels
+        strip.text.x = element_text(size = 25) # size of facet text
+  ) 
+
+ggsave("Output/DML/multi_plot_dml_boxplot_ate.png", dml_ate_boxplot_multi)
+
+
+# ATTE
+df_dml_results_ate_boxplot_multi <- df_dml_results_boxplot_multi %>% filter(Type == "ATTE")
+dml_atte_boxplot_multi <- 
+  ggplot(df_dml_results_ate_boxplot_multi, aes(x = ML_algo, y = Treatment_Effect)) +
+  geom_boxplot(fill = "grey") +
+  #xlab("\nMachine Learning Algorithms\n") +
+  xlab("") + ylab("\nATTE\n") +
+  facet_wrap(~ Treatment) + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 28), # size of x-axis tick labels
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1), # rotate x-axis ticks
+        axis.title = element_text(size = 30, face = "bold"), # size of x-axis labels
+        strip.text.x = element_text(size = 25) # size of facet text
+  ) 
+
+
+ggsave("Output/DML/multi_plot_dml_boxplot_atte.png", dml_atte_boxplot_multi)
+
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%#
@@ -282,7 +449,7 @@ ggsave("Output/DML/binary_plot_dml_boxplot.png", dml_boxplot_binary)
 
 ## BINARY TREATMENT SETTING ##
 
-# lasso
+# (post-)lasso
 df_dml_hyperparam_all_binary_lasso <- data.frame()
 for (model_algo_sel in c("lasso", "postlasso")) {
   df_dml_load <- readRDS(paste0("Output/DML/", model_algo_sel, "_", load_results_main, ".rds"))
@@ -322,6 +489,47 @@ for (i in 1:5) {
 df_dml_hyperparam_all_binary_xgb <- rbind(df_dml_hyperparam_all_binary_xgb, df_dml_hyperparam_sub_all)
 
 
+
+
+
+## MULTIVALUED TREATMENT SETTING ##
+
+# (post-)lasso
+df_dml_hyperparam_all_multi_lasso <- data.frame()
+for (model_algo_sel in c("lasso", "postlasso")) {
+  df_dml_load <- readRDS(paste0("Output/DML/multi_", model_algo_sel, "_", load_results_main, ".rds"))
+  df_dml_hyperparam_sub_all <- data.frame()
+  for (i in 1:5) {
+    df_dml_hyperparam_sub <- df_dml_load[[i]]$param %>% mutate(MICE = i) %>% mutate(mlalgo = model_algo_sel)
+    df_dml_hyperparam_sub_all <- rbind(df_dml_hyperparam_sub_all, df_dml_hyperparam_sub)
+  }
+  
+  df_dml_hyperparam_all_multi_lasso <- rbind(df_dml_hyperparam_all_multi_lasso, df_dml_hyperparam_sub_all)
+  
+}
+
+
+# random forests (no parameter tuning)
+df_dml_hyperparam_all_multi_rf <- data.frame()
+
+df_dml_load <- readRDS(paste0("Output/DML/multi_", "randomforests", "_", load_results_main, ".rds"))
+df_dml_hyperparam_sub_all <- data.frame()
+for (i in 1:5) {
+  df_dml_hyperparam_sub <- df_dml_load[[i]]$param %>% mutate(MICE = i) %>% mutate(mlalgo = "randomforests")
+  df_dml_hyperparam_sub_all <- rbind(df_dml_hyperparam_sub_all, df_dml_hyperparam_sub)
+}
+df_dml_hyperparam_all_multi_rf <- rbind(df_dml_hyperparam_all_multi_rf, df_dml_hyperparam_sub_all)
+
+
+# xgboost
+df_dml_hyperparam_all_multi_xgb <- data.frame()
+df_dml_load <- readRDS(paste0("Output/DML/multi_", "xgboost", "_", load_results_main, ".rds"))
+df_dml_hyperparam_sub_all <- data.frame()
+for (i in 1:5) {
+  df_dml_hyperparam_sub <- df_dml_load[[i]]$param %>% mutate(MICE = i) %>% mutate(mlalgo = "xgboost")
+  df_dml_hyperparam_sub_all <- rbind(df_dml_hyperparam_sub_all, df_dml_hyperparam_sub)
+}
+df_dml_hyperparam_all_multi_xgb <- rbind(df_dml_hyperparam_all_multi_xgb, df_dml_hyperparam_sub_all)
 
 
 
