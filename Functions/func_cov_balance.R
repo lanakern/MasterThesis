71fc7635c8aaa558fb7374fa4a7d92681818c6e6
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-03-03 13:12:28 +0100

    Add for all covariates and for selected

--- Functions/func_cov_balance.R
+++ Functions/func_cov_balance.R
@@ -2,7 +2,7 @@
 #### FUNCTION: COVARIATE BALANCING AND MAIN DRIVERS OF SELECTION ####
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
-func_cov_balance <- function(treatment_setting, data, weights) {
+func_cov_balance <- function(treatment_setting, data, controls, weights) {
   
   
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
@@ -14,9 +14,11 @@ func_cov_balance <- function(treatment_setting, data, weights) {
     #### Before ####
     #++++++++++++++#
     
-    controls_drop <- data %>% select(starts_with("treatment"), starts_with("outcome"), group) %>% colnames()
+    # drop controls not needed
+    controls_drop <- data %>% 
+      select(starts_with("treatment"), starts_with("outcome"), group) %>% 
+      colnames()
     controls_drop <- controls_drop[controls_drop != "treatment_sport"]
-    
     data_binary_bef <- data %>% select(-all_of(controls_drop))
 
     # calculate absolute difference in means
@@ -46,6 +48,7 @@ func_cov_balance <- function(treatment_setting, data, weights) {
     num_cols <- ncol(data_binary_bef) - 1
     df_smd_binary_bef <- data.frame(
       "treatment_setting" = "binary", "adjustment" = "before", 
+      "controls" = "all",
       "min" = min(df_smd_bef_binary$SD), "max" = max(df_smd_bef_binary$SD), 
       "mean" = mean(df_smd_bef_binary$SD), "median" = median(df_smd_bef_binary$SD),
       "num_cov_smd_20" = sum(df_smd_bef_binary > 20), 
@@ -60,7 +63,12 @@ func_cov_balance <- function(treatment_setting, data, weights) {
     #### After ####
     #+++++++++++++#
     
-    data_binary_after <- as.data.frame(as.matrix(data_binary_bef) * weights)
+    # for selected covariates #
+    #-------------------------#
+    
+    controls <- controls[controls %in% colnames(data_binary_bef)]
+    data_binary_after <- data_binary_bef %>% select(all_of(controls))
+    data_binary_after <- as.data.frame(as.matrix(data_binary_after) * weights)
     data_binary_after$treatment_sport <- data_binary_bef$treatment_sport
     
     # calculate absolute difference in means
@@ -73,14 +81,15 @@ func_cov_balance <- function(treatment_setting, data, weights) {
       as.matrix()
     
     
-    # calculate factor
-    df_meanvar_binary_after <- data_binary_after %>% 
-      group_by(treatment_sport) %>%
-      summarise(across(everything(), var)) %>%
-      select(-treatment_sport) %>%
-      summarise(across(everything(), mean)) %>% 
-      summarise(across(everything(), sqrt)) %>%
-      as.matrix()
+    # calculate factor: same vector as for before
+    df_meanvar_binary_after <- df_meanvar_binary_bef[, controls]
+    # df_meanvar_binary_after <- data_binary_after %>%
+    #   group_by(treatment_sport) %>%
+    #   summarise(across(everything(), var)) %>%
+    #   select(-treatment_sport) %>%
+    #   summarise(across(everything(), mean)) %>%
+    #   summarise(across(everything(), sqrt)) %>%
+    #   as.matrix()
     
     
     # fraction
@@ -91,7 +100,8 @@ func_cov_balance <- function(treatment_setting, data, weights) {
     # descriptives
     num_cols <- ncol(data_binary_after) - 1
     df_smd_binary_after <- data.frame(
-      "treatment_setting" = "binary", "adjustment" = "before", 
+      "treatment_setting" = "binary", "adjustment" = "after", 
+      "controls" = "selected",
       "min" = min(df_smd_after_binary$SD), 
       "max" = max(df_smd_after_binary$SD), 
       "mean" = mean(df_smd_after_binary$SD), 
@@ -106,6 +116,64 @@ func_cov_balance <- function(treatment_setting, data, weights) {
     
     df_smd <- rbind(df_smd_binary_bef, df_smd_binary_after)
     
+    
+    # for all covariates #
+    #--------------------#
+    
+    data_binary_after <- as.data.frame(as.matrix(data_binary_bef) * weights)
+    data_binary_after$treatment_sport <- data_binary_bef$treatment_sport
+    
+    # calculate absolute difference in means
+    df_diff_binary_after <- abs(diff(as.matrix(
+      data_binary_after %>% 
+        group_by(treatment_sport) %>%
+        summarise(across(everything(), mean)) %>%
+        select(-treatment_sport)
+    ))) %>% 
+      as.matrix()
+    
+    
+    # calculate factor: same vector as for before
+    df_meanvar_binary_after <- df_meanvar_binary_bef[, controls]
+    # df_meanvar_binary_after <- data_binary_after %>%
+    #   group_by(treatment_sport) %>%
+    #   summarise(across(everything(), var)) %>%
+    #   select(-treatment_sport) %>%
+    #   summarise(across(everything(), mean)) %>%
+    #   summarise(across(everything(), sqrt)) %>%
+    #   as.matrix()
+    
+    
+    # fraction
+    df_smd_after_binary <- data.frame(
+      "SD" = t((df_diff_binary_after / df_meanvar_binary_after) * 100)
+    )
+    
+    # descriptives
+    num_cols <- ncol(data_binary_after) - 1
+    df_smd_binary_after <- data.frame(
+      "treatment_setting" = "binary", "adjustment" = "after", 
+      "controls" = "all",
+      "min" = min(df_smd_after_binary$SD), 
+      "max" = max(df_smd_after_binary$SD), 
+      "mean" = mean(df_smd_after_binary$SD), 
+      "median" = median(df_smd_after_binary$SD),
+      "num_cov_smd_20" = sum(df_smd_after_binary > 20), 
+      "num_cov_smd_10" = sum(df_smd_after_binary > 10), 
+      "num_cov_smd_5" = sum(df_smd_after_binary > 5),
+      "perc_cov_smd_20" = sum(df_smd_after_binary > 20) / num_cols, 
+      "perc_cov_smd_10" = sum(df_smd_after_binary > 10) / num_cols, 
+      "perc_cov_smd_5" = sum(df_smd_after_binary > 5) / num_cols
+    )
+    
+    
+    df_smd <- rbind(df_smd, df_smd_binary_after)
+    df_smd_all <- left_join(
+      rownames_to_column(df_smd_bef_binary) %>% rename("SD_before" = SD),
+      rownames_to_column(df_smd_after_binary) %>% rename("SD_after" = SD),
+      by = "rowname"
+    ) %>% rename("control_var" = "rowname")
+    
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
   #### MULTIVALUED TREATMENT SETTING ####
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%# 
@@ -116,6 +184,6 @@ func_cov_balance <- function(treatment_setting, data, weights) {
   
 
   # return data frame
-  return(df_smd)
+  return(list("smd_summary" = df_smd, "smd_values" = df_smd_all))
 
 }
