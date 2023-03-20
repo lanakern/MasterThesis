0d62849b748b15cd9891f2cf920405c3a06f3264
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-03-20 10:36:38 +0100

    Correct plot for multivalued treatment setting

--- Functions/func_dml_common_support.R
+++ Functions/func_dml_common_support.R
@@ -35,7 +35,7 @@ func_dml_common_support <- function(treatment_setting, data_pred, min_trimming, 
       mutate(treatment_label = ifelse(treatment == 1, "Sport Participatiopn", "No Sport Participation")) %>%
       ggplot(aes(x = m, fill = treatment_label)) +
       # histogram
-      geom_histogram(aes(y = ..density..), binwidth = 0.01) +
+      geom_histogram(aes(y = ..density..), binwidth = 0.01,  alpha = 0.4, color = "black") +
       scale_fill_manual(values = c("grey88", "grey38")) +
       # trimming thresholds
       geom_vline(xintercept = min_trimming, linetype = "longdash", 
@@ -56,32 +56,88 @@ func_dml_common_support <- function(treatment_setting, data_pred, min_trimming, 
     
   } else if (treatment_setting == "multi") {
     
-    # generate plot
-    data_pred %>%
-      mutate(treatment_label = case_when(treatment == 1 ~ "Daily and weekly", 
-                                         treatment == 2 ~ "Monthly or less frequently",
-                                         TRUE ~ "Never")) %>%
-      gather(m1, m2, m3, key = "class", value = "prob") %>% 
-      mutate(class = case_when(class == "m1" ~ "Daily + Weekly", 
-                               class == "m2" ~ "Monthly or less", 
-                               TRUE ~ "Never")) %>%
-      ggplot() +
+    # prepare data
+    data_pred_m1 <- data_pred %>%
+      dplyr::select(treatment, m1) %>%
+      mutate(treatment_label = case_when(
+        treatment == 1 ~ "Yes", TRUE ~ "No"
+      ))
+    
+    data_pred_m2 <- data_pred %>%
+      dplyr::select(treatment, m2) %>%
+      mutate(treatment_label = case_when(
+        treatment == 2 ~ "Yes", TRUE ~ "No"
+      ))
+    
+    data_pred_m3 <- data_pred %>%
+      dplyr::select(treatment, m3) %>%
+      mutate(treatment_label = case_when(
+        treatment == 3 ~ "Yes", TRUE ~ "No"
+      ))
+    
+    
+    # generate plots
+    plot_m1 <- 
+      data_pred_m1 %>%
+      ggplot(aes(x = m1, fill = treatment_label)) +
       # histogram
-      geom_histogram(aes(y = ..density.., x = prob, fill = treatment_label), alpha = 0.6, binwidth = 0.01) +
+      geom_histogram(aes(y = ..density..), binwidth = 0.01,  alpha = 0.4, color = "black") +
       # bar colors
-      scale_fill_manual(values = c("darkgreen", "grey38", "darkblue")) +
+      scale_fill_manual(values = c("grey88", "grey38")) +
       # trimming thresholds
       geom_vline(xintercept = min_trimming, linetype = "longdash", color = "black", size = 0.5) +
       geom_vline(xintercept = max_trimming, linetype = "longdash", color = "black", size = 0.5) + 
-      # facet
-      facet_wrap(~ class, ncol = 1) +
       # aesthetics
       xlab("Propensity Score") + 
       ylab("Density") + 
-      ggtitle("Propensity Score Overlap") +
+      ggtitle(paste("Propensity Score Overlap for Prediction of Weekly Sport Participation")) +
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5)) +
       guides(fill = guide_legend(title = "Treatment Group"))
+    
+    
+    plot_m2 <- 
+      data_pred_m2 %>%
+      ggplot(aes(x = m2, fill = treatment_label)) +
+      # histogram
+      geom_histogram(aes(y = ..density..), binwidth = 0.01,  alpha = 0.4, color = "black") +
+      # bar colors
+      scale_fill_manual(values = c("grey88", "grey38")) +
+      # trimming thresholds
+      geom_vline(xintercept = min_trimming, linetype = "longdash", color = "black", size = 0.5) +
+      geom_vline(xintercept = max_trimming, linetype = "longdash", color = "black", size = 0.5) + 
+      # aesthetics
+      xlab("Propensity Score") + 
+      ylab("Density") + 
+      ggtitle(paste("Propensity Score Overlap for Prediction of Monthly Sport Participation")) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      guides(fill = guide_legend(title = "Treatment Group"))
+    
+    
+    plot_m3 <-
+      data_pred_m3 %>%
+      ggplot(aes(x = m3, fill = treatment_label)) +
+      # histogram
+      geom_histogram(aes(y = ..density..), binwidth = 0.01,  alpha = 0.4, color = "black") +
+      # bar colors
+      scale_fill_manual(values = c("grey88", "grey38")) +
+      # trimming thresholds
+      geom_vline(xintercept = min_trimming, linetype = "longdash", color = "black", size = 0.5) +
+      geom_vline(xintercept = max_trimming, linetype = "longdash", color = "black", size = 0.5) + 
+      # aesthetics
+      xlab("Propensity Score") + 
+      ylab("Density") + 
+      ggtitle(paste("Propensity Score Overlap for Prediction of No Sport Participation")) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      guides(fill = guide_legend(title = "Treatment Group"))
+    
+    
+    # combine plots
+    grid.arrange(plot_m1, plot_m2, plot_m3, nrow = 3)
+    
+    
   }
   
 
