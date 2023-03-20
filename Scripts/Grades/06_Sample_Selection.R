c6471441de16b551531f1bf38e3861fda67a4b31
LanaKern <lana.kern@online.de>
2023-03-20 14:41:42 +0100

    Add calculations for lags

--- Scripts/Grades/06_Sample_Selection.R
+++ Scripts/Grades/06_Sample_Selection.R
@@ -58,9 +58,10 @@ data_sub_1 <- data_raw
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
 # select column names containing info of extracurricular activity
-colnames_extra <- data_sub_1 %>%
+colnames_extra_all <- data_sub_1 %>%
   dplyr::select(matches("extracurricular_") & !ends_with("freq"), "interest_music_play") %>%
   colnames()
+colnames_extra <- colnames_extra_all[!str_detect(colnames_extra_all, "_lag")] # no lags
 
 # generate new data frame
 data_count <- data.frame()
@@ -94,6 +95,29 @@ data_count <- data_sub_1 %>%
 summary(data_count$extracurricular_num)
 
 
+# do the same for the lags
+colnames_extra_lag <- colnames_extra_all[str_detect(colnames_extra_all, "_lag")] # no lags
+colnames_extra_lag <- colnames_extra_lag[!str_detect(colnames_extra_lag, "_freq_")] # no freq
+colnames_extra_lag <- colnames_extra_lag[!str_detect(colnames_extra_lag, "_NA")] # no NA
+
+data_count_lags <- data_sub_1 %>%
+  ungroup() %>%
+  dplyr::select(ID_t, treatment_period, all_of(colnames_extra_lag)) %>%
+  mutate_at(vars(colnames_extra_lag), ~ as.integer(.)) %>%
+  mutate(extracurricular_num_lag = rowSums(dplyr::select(., all_of(colnames_extra_lag)), na.rm = TRUE)) %>%
+  mutate(sum_na = rowSums(is.na(dplyr::select(., all_of(colnames_extra_lag))))) %>%
+  mutate(extracurricular_num_lag = case_when(sum_na == length(colnames_extra_lag) ~ as.double(NA), 
+                                             TRUE ~ extracurricular_num_lag)) %>%
+  dplyr::select(ID_t, treatment_period, extracurricular_num_lag) %>%
+  arrange(ID_t, treatment_period) %>%
+  group_by(ID_t) %>% fill(extracurricular_num_lag, .direction = "down") %>% ungroup() %>%
+  mutate(extracurricular_lag_NA = ifelse(is.na(extracurricular_num_lag), 1, 0)) %>% 
+  arrange(ID_t, treatment_period) %>%
+  distinct() %>% ungroup()
+
+summary(data_count_lags$extracurricular_num_lag)
+
+
 # Subset #
 #++++++++#
 
@@ -103,10 +127,20 @@ if (extra_act == "yes") {
   data_sub_2 <- left_join(
     data_sub_1, data_count, by = c("ID_t", "treatment_period")
   ) %>%
-    filter(extracurricular_num > 0 | treatment_sport == 1)
+    filter(extracurricular_num > 0 | treatment_sport == 1) %>%
+    filter(!is.na(extracurricular_num))
+  
+  data_sub_2 <- left_join(
+    data_sub_2, data_count_lags, by = c("ID_t", "treatment_period")
+  )
+  
 } else {
   data_sub_2 <- left_join(
     data_sub_1, data_count, by = c("ID_t", "treatment_period")
+  )
+  
+  data_sub_2 <- left_join(
+    data_sub_2, data_count_lags, by = c("ID_t", "treatment_period")
   )
 }
 
@@ -115,20 +149,20 @@ id_num_adj_1 <- length(unique(data_sub_2$ID_t))
 id_treatment_periods_1 <- nrow(data_sub_2)
 id_num_drop <- id_num_start - id_num_adj_1
 
-# set NA in extracurricular_num to zero
+# set NA in extracurricular_num to zero 
 table(data_sub_2$extracurricular_num, useNA = "always")
-data_sub_2 <- data_sub_2 %>%
-  mutate(extracurricular_num = case_when(
-    is.na(extracurricular_num) ~ 0, TRUE ~ extracurricular_num
-  ))
-table(data_sub_2$extracurricular_num, useNA = "always")
-
-# sample size if NA would not be dropped
-left_join(
-  data_sub_1, data_count, by = c("ID_t", "treatment_period")
-) %>%
-  filter(extracurricular_num > 0  | is.na(extracurricular_num)) %>%
-  pull(ID_t) %>% unique() %>% length()
+table(data_sub_2$extracurricular_num_lag, useNA = "always")
+# data_sub_2 <- data_sub_2 %>%
+#   mutate(
+#     extracurricular_num_lag_NA = ifelse(is.na(extracurricular_num_lag), 1, 0)
+#   ) %>% 
+#   mutate(
+#     extracurricular_num = case_when(
+#       is.na(extracurricular_num) ~ 0, TRUE ~ extracurricular_num),
+#     extracurricular_num_lag = case_when(
+#       is.na(extracurricular_num_lag) ~ 0, TRUE ~ extracurricular_num_lag)
+#   )
+# table(data_sub_2$extracurricular_num, useNA = "always")
 
 
 
@@ -189,6 +223,47 @@ data_sub_2 <- left_join(
   data_sub_2, data_freq, by = c("ID_t", "treatment_period")
 )
 
+# do the same for lags
+colnames_extra_freq_lag <- data_sub_2 %>%
+  dplyr::select(matches("extracurricular_") & ends_with("freq_lag"), "interest_music_play_freq_lag") %>%
+  colnames()
+
+table(data_sub_2$interest_music_play_freq_lag)
+data_sub_2 <- data_sub_2 %>%
+  mutate(interest_music_play_freq_lag_orig = interest_music_play_freq_lag) %>%
+  mutate(interest_music_play_freq_lag = case_when(
+    interest_music_play_freq_lag >= 28 ~ 1, 
+    interest_music_play_freq_lag < 28 & interest_music_play_freq_lag > 5 ~ 2,
+    interest_music_play_freq_lag %in% c(4,5) ~ 3, 
+    interest_music_play_freq_lag < 4 & interest_music_play_freq_lag > 1 ~ 4, 
+    interest_music_play_freq_lag == 1 ~ 5, 
+    interest_music_play_freq_lag == 0 ~ 6,
+    is.na(interest_music_play_freq_lag) ~ as.double(NA), TRUE ~ 7
+  ))
+table(data_sub_2$interest_music_play_freq_lag)
+
+data_freq_lags <- data_sub_2 %>%
+  ungroup() %>%
+  dplyr::select(ID_t, treatment_period, all_of(colnames_extra_freq_lag)) %>%
+  mutate(extracurricular_freq_lag = pmax(!!!rlang::syms(colnames_extra_freq_lag), na.rm = TRUE)) %>%
+  dplyr::select(ID_t, treatment_period, extracurricular_freq_lag) %>%
+  mutate(extracurricular_freq_lag = case_when(
+    extracurricular_freq_lag == 1 ~ "daily",
+    extracurricular_freq_lag == 2 ~ "several times a week",
+    extracurricular_freq_lag == 3 ~ "once a week",
+    extracurricular_freq_lag == 4 ~ "several times a month",
+    extracurricular_freq_lag == 5 ~ "once a month",
+    extracurricular_freq_lag == 6 ~ "less frequently",
+    is.na(extracurricular_freq_lag) ~ as.character(NA), TRUE ~ "never")) %>%
+  group_by(ID_t) %>% fill(extracurricular_freq_lag, .direction = "down") %>% ungroup()
+
+table(data_freq_lags$extracurricular_freq_lag, useNA = "always")
+sum(duplicated(data_freq_lags %>% dplyr::select(ID_t, treatment_period)))
+
+
+data_sub_2 <- left_join(
+  data_sub_2, data_freq_lags, by = c("ID_t", "treatment_period")
+)
 
 
 ## Type of Extracurricular Activity ##
@@ -215,8 +290,30 @@ data_sub_2 <- left_join(data_sub_2, df_extracurricular_uni,
                         by = c("ID_t", "treatment_period"))
 
 
+cols_extra_uni_lag <- 
+  c("extracurricular_association_lag", "extracurricular_committee_lag", 
+    "extracurricular_action_lag", "extracurricular_fraternal_lag", "extracurricular_culture_lag", 
+    "extracurricular_political_lag")#, "extracurricular_other")
+
+df_extracurricular_uni_lag <- data_sub_2 %>% 
+  dplyr::select(ID_t, treatment_period, all_of(cols_extra_uni_lag)) %>%
+  mutate(check_uni = rowSums(dplyr::select(., all_of(cols_extra_uni_lag)), na.rm = TRUE)) %>%
+  mutate(check_uni_sum_na = rowSums(is.na(dplyr::select(., all_of(cols_extra_uni_lag))))) %>%
+  mutate(extracurricular_uni_lag = case_when(
+    check_uni_sum_na == length(cols_extra_uni_lag) ~ as.double(NA), 
+    check_uni == 0 & check_uni_sum_na != length(cols_extra_uni_lag) ~ 0, 
+    TRUE ~ 1)) %>%
+  dplyr::select(-c(check_uni, check_uni_sum_na)) %>%
+  dplyr::select(ID_t, treatment_period, extracurricular_uni_lag) %>%
+  group_by(ID_t) %>% fill(extracurricular_uni_lag, .direction = "down") %>% ungroup() 
+
+data_sub_2 <- left_join(data_sub_2, df_extracurricular_uni_lag, 
+                        by = c("ID_t", "treatment_period"))
+
+
 # outside
 cols_extra_uni_no <- data_sub_2 %>% dplyr::select(matches("extracurricular_leisure") & !ends_with("freq")) %>% colnames()
+cols_extra_uni_no <- cols_extra_uni_no[!str_detect(cols_extra_uni_no, "_lag")]
 
 df_extracurricular_uni_no <- data_sub_2 %>% 
   dplyr::select(ID_t, treatment_period, all_of(cols_extra_uni_no)) %>%
@@ -224,7 +321,7 @@ df_extracurricular_uni_no <- data_sub_2 %>%
   mutate(check_uni_sum_na = rowSums(is.na(dplyr::select(., all_of(cols_extra_uni_no))))) %>%
   mutate(extracurricular_uni_no = case_when(
     check_uni_sum_na == length(cols_extra_uni_no) ~ as.double(NA), 
-    check_uni == 0 & check_uni_sum_na != length(cols_extra_uni) ~ 0, TRUE ~ 1)) %>%
+    check_uni == 0 & check_uni_sum_na != length(cols_extra_uni_no) ~ 0, TRUE ~ 1)) %>%
   dplyr::select(-c(check_uni, check_uni_sum_na)) %>%
   dplyr::select(ID_t, treatment_period, extracurricular_uni_no) %>%
   group_by(ID_t) %>% fill(extracurricular_uni_no, .direction = "down") %>% ungroup() 
@@ -233,17 +330,44 @@ data_sub_2 <- left_join(data_sub_2, df_extracurricular_uni_no,
                         by = c("ID_t", "treatment_period"))
 
 
+cols_extra_uni_no_lag <- data_sub_2 %>% dplyr::select(matches("extracurricular_leisure") & !contains("freq") & ends_with("lag")) %>% colnames()
+
+df_extracurricular_uni_no_lag <- data_sub_2 %>% 
+  dplyr::select(ID_t, treatment_period, all_of(cols_extra_uni_no_lag)) %>%
+  mutate(check_uni = rowSums(dplyr::select(., all_of(cols_extra_uni_no_lag)), na.rm = TRUE)) %>%
+  mutate(check_uni_sum_na_lag = rowSums(is.na(dplyr::select(., all_of(cols_extra_uni_no_lag))))) %>%
+  mutate(extracurricular_uni_no_lag = case_when(
+    check_uni_sum_na_lag == length(cols_extra_uni_no_lag) ~ as.double(NA), 
+    check_uni == 0 & check_uni_sum_na_lag != length(cols_extra_uni_no_lag) ~ 0, TRUE ~ 1)) %>%
+  dplyr::select(-c(check_uni, check_uni_sum_na_lag)) %>%
+  dplyr::select(ID_t, treatment_period, extracurricular_uni_no_lag) %>%
+  group_by(ID_t) %>% fill(extracurricular_uni_no_lag, .direction = "down") %>% ungroup() 
+
+data_sub_2 <- left_join(data_sub_2, df_extracurricular_uni_no_lag, 
+                        by = c("ID_t", "treatment_period"))
+
+
 # music
 data_sub_2 <- data_sub_2 %>%
   mutate(extracurricular_music = case_when(
     interest_music_play == 1 ~ 1, is.na(interest_music_play) ~ as.double(NA),
     TRUE ~ 0
-  ))
+  ),
+  extracurricular_music_lag = case_when(
+    interest_music_play_lag == 1 ~ 1, is.na(interest_music_play_lag) ~ as.double(NA),
+    TRUE ~ 0
+  )
+  )
 
 
 table(data_sub_2$extracurricular_uni, useNA = "always")
 table(data_sub_2$extracurricular_uni_no, useNA = "always")
 table(data_sub_2$extracurricular_music, useNA = "always")
+
+table(data_sub_2$extracurricular_uni_lag, useNA = "always")
+table(data_sub_2$extracurricular_uni_no_lag, useNA = "always")
+table(data_sub_2$extracurricular_music_lag, useNA = "always")
+
 
 # checks
 check_uni <- data_sub_2 %>%
@@ -264,8 +388,11 @@ check_num <- data_sub_2 %>%
 
 # drop all extracurricular variables not needed anymore
 colnames_extra <- data_sub_2 %>% dplyr::select(starts_with("extracurricular")) %>% colnames()
-colnames_extra_keep <- c("extracurricular_freq", "extracurricular_num", 
-                         "extracurricular_uni", "extracurricular_uni_no", "extracurricular_music")
+colnames_extra_keep <- c("extracurricular_freq", "extracurricular_freq_lag", 
+                         "extracurricular_num", "extracurricular_num_lag",
+                         "extracurricular_uni", "extracurricular_uni_lag", 
+                         "extracurricular_uni_no", "extracurricular_uni_no_lag",
+                         "extracurricular_music", "extracurricular_music_lag")
 colnames_extra_drop <- colnames_extra[!colnames_extra %in% colnames_extra_keep]
 
 
@@ -274,6 +401,20 @@ data_sub_2 <- data_sub_2 %>%
   dplyr::select(-(all_of(colnames_extra_drop)))
 
 
+# aggregate extracurricular freq variables
+data_sub_2 <- data_sub_2 %>%
+  mutate(extracurricular_freq = case_when(
+    extracurricular_freq == "daily" | str_detect(extracurricular_freq, "week") ~ "weekly",
+    str_detect(extracurricular_freq, "month")  ~ "monthly",
+    extracurricular_freq == "less frequently" ~ "less frequently",
+    TRUE ~ as.character(NA)
+  ),
+  extracurricular_freq_lag = case_when(
+    extracurricular_freq_lag == "daily" | str_detect(extracurricular_freq_lag, "week") ~ "weekly",
+    str_detect(extracurricular_freq_lag, "month")  ~ "monthly",
+    extracurricular_freq_lag == "less frequently" ~ "less frequently",
+    TRUE ~ as.character(NA)
+  ))
 
 
 #%%%%%%%%%%%#
