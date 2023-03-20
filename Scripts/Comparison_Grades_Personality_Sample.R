71d87062e4bfd781c4a96fe0bf2a6fc79cf41e28
LanaKern <lana.kern@online.de>
2023-03-20 14:40:14 +0100

    Compare data frame columns across mice data sets

--- Scripts/Comparison_Grades_Personality_Sample.R
+++ Scripts/Comparison_Grades_Personality_Sample.R
@@ -18,3 +18,56 @@ outersect <- function(x, y) {
 length(outersect(id_grades, id_pers))
 length(outersect(id_grades, id_pers)) / num_id_grades
 1 - length(outersect(id_grades, id_pers)) / num_id_grades
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#### Comparison Data Sets ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+data_main_mice1 <- readRDS("Data/Grades/Prep_10/prep_10_dml_binary_all_weekly_down_extradrop_mice1.rds")
+data_main_mice2 <- readRDS("Data/Grades/Prep_10/prep_10_dml_binary_all_weekly_down_extradrop_mice2.rds")
+data_main_mice3 <- readRDS("Data/Grades/Prep_10/prep_10_dml_binary_all_weekly_down_extradrop_mice3.rds")
+data_main_mice4 <- readRDS("Data/Grades/Prep_10/prep_10_dml_binary_all_weekly_down_extradrop_mice4.rds")
+data_main_mice5 <- readRDS("Data/Grades/Prep_10/prep_10_dml_binary_all_weekly_down_extradrop_mice5.rds")
+
+setdiff(colnames(data_main_mice1), colnames(data_main_mice2))
+setdiff(colnames(data_main_mice2), colnames(data_main_mice3))
+setdiff(colnames(data_main_mice4), colnames(data_main_mice5))
+
+
+# check all variables
+summary(data_main_mice1$outcome_grade_lag)
+summary(data_main_mice1$treatment_sport_lag)
+data_main_mice1 %>% dplyr::select(starts_with("outcome")) %>% colnames()
+data_main_mice1 %>% dplyr::select(starts_with("treatment")) %>% colnames()
+
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("outcome"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("bigfive"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("treatment"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("interview"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("birth"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("health"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("educ"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("opinion_educ"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("academic"), starts_with("helpless"), starts_with("motivation"), starts_with("prof_expected"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("uni"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("extracurricular"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("interest"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("comp"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("emp"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("father"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("mother"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("parents"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("friends"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("personality"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("sibling"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("partner"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("child"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("living"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-starts_with("prof_expected"))
+data_main_mice1 <- data_main_mice1 %>% dplyr::select(-c(starts_with("gender"), starts_with("age"), bilingual, kindergarden,
+                                                migration, starts_with("religion"),
+                                                academic, starts_with("helpless_grades"), starts_with("motivation_degree"),
+                                                starts_with("satisfaction"), starts_with("social_integr"), starts_with("stress"),
+                                                starts_with("status_prof"), starts_with("place_residence"), starts_with("na")))
+
