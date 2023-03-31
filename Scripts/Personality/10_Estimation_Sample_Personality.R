dda56c67eaa5fd9e5960ed552f1b5da7e68bae7f
LanaKern <lana.kern@online.de>
2023-03-31 12:19:32 +0200

    Create interaction terms at beginning of the file

--- Scripts/Personality/10_Estimation_Sample_Personality.R
+++ Scripts/Personality/10_Estimation_Sample_Personality.R
@@ -90,6 +90,83 @@ for (mice_data_sel in 1:5) {
   # ensure all constant variables are dropped
   data_final <- remove_constant(data_final)
   
+  # create interactions
+  if (create_interactions == "yes") {
+    # create new data frame
+    data_all_plus <- data_final
+    
+    #-- INTERACTION TERMS --#
+    
+    ## 1.) Drop treatment and outcome variables which are not used to generate interactions
+    df_interaction <- data_all_plus %>% 
+      dplyr::select(-c(starts_with("treatment"), starts_with("bigfive"), group))
+    ## 2.) Generate interactions
+    # https://stackoverflow.com/questions/31905221/r-generate-all-possible-interaction-variables
+    df_interaction <- do.call(cbind, combn(colnames(df_interaction), 2, FUN = function(x)
+      list(setNames(data.frame(df_interaction[,x[1]]*df_interaction[,x[2]]),
+                    paste(x, collapse = ":")))))
+    seq_interaction <- seq(10000, ncol(df_interaction), 10000)
+    seq_interaction[length(seq_interaction)] <- ncol(df_interaction)
+    seq_interaction_start <- 1
+    interaction_drop_all <- c()
+    for (col_num_max in seq_interaction) {
+      interaction_col_sums <- colSums(df_interaction[,seq_interaction_start:col_num_max] == 0) / nrow(df_interaction)
+      interaction_drop <- names(interaction_col_sums[interaction_col_sums >= 0.80])
+      interaction_drop_all <- c(interaction_drop_all, interaction_drop)
+      
+      seq_interaction_start <- col_num_max + 1
+    }
+    
+    df_interaction <- df_interaction %>% 
+      dplyr::select(-all_of(interaction_drop_all)) 
+    ## 3.) Add interactions to full data frame
+    data_all_plus <- cbind(data_all_plus, df_interaction)
+    ## 4.) Drop interactions containing 80% or more empty cells
+    ## only for those with at least 80% of zeros this is operation relevant
+    # colnames_interaction <- colnames(
+    #   df_interaction[, which(as.numeric(colSums(df_interaction == 0) / nrow(df_interaction)) >= 0.80)]
+    # )
+    # #colnames_interaction_sub <- colnames_interaction[1:10000]
+    # gc()
+    # i <- 0
+    # for (colnames_interaction_sel in colnames_interaction) {
+    #   i <- i + 1
+    #   if (i %% 5000 == 0) {print(paste("Iteration", i))}
+    #   
+    #   colnames_interaction_sel_1 <- str_split(colnames_interaction_sel, ":")[[1]][1]
+    #   colnames_interaction_sel_2 <- str_split(colnames_interaction_sel, ":")[[1]][2]
+    #   num_rows_interaction <- 
+    #     data_all_plus %>% 
+    #     dplyr::select(all_of(colnames_interaction_sel), 
+    #                   all_of(colnames_interaction_sel_1), 
+    #                   all_of(colnames_interaction_sel_2)) %>%
+    #     filter(!!rlang::sym(colnames_interaction_sel_1) == 0 & 
+    #              !!rlang::sym(colnames_interaction_sel_2) == 0) %>%
+    #     nrow()
+    #   num_rows_interaction <- num_rows_interaction / nrow(data_all_plus)
+    #   if (num_rows_interaction >= 0.80) {
+    #     data_all_plus <- data_all_plus %>% dplyr::select(-all_of(colnames_interaction_sel))
+    #   } else {
+    #     data_all_plus <- data_all_plus
+    #   }
+    #   
+    # }
+
+    # generate polynominals
+    cols_numeric_all <- names(unlist(lapply(data_final, class)[lapply(data_final, class) == "numeric"]))
+    
+    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "outcome")]
+    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "treatment")]
+    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "na_")]
+    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "group")]
+    
+    data_all_plus <- data_all_plus %>%
+      mutate(across(all_of(cols_numeric_all), .fns = list("order2" = ~ .^2))) %>%
+      mutate(across(all_of(cols_numeric_all), .fns = list("order3" = ~ .^3))) %>%
+      mutate(across(all_of(cols_numeric_all), .fns = list("order4" = ~ .^4)))
+    
+    data_all_plus <- remove_constant(data_all_plus)
+  }
   
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
   #### BINARY TREATMENT SETTING ####
@@ -171,79 +248,20 @@ for (mice_data_sel in 1:5) {
   #++++++++++++++++++++++++++++++++++++++++#
 
   if (create_interactions == "yes") {
+    
     # create new data frame
-    data_all_plus <- data_binary
+    data_binary_all_plus <- data_all_plus %>% dplyr::select(-starts_with("treatment_sport_freq"))
     
-    #-- INTERACTION TERMS --#
-    
-    ## 1.) Drop treatment and outcome variables which are not used to generate interactions
-    df_interaction <- data_all_plus %>% 
-      dplyr::select(-c(starts_with("treatment"), starts_with("bigfive"), group))
-    ## 2.) Generate interactions
-    # https://stackoverflow.com/questions/31905221/r-generate-all-possible-interaction-variables
-    df_interaction <- do.call(cbind, combn(colnames(df_interaction), 2, FUN = function(x)
-      list(setNames(data.frame(df_interaction[,x[1]]*df_interaction[,x[2]]),
-                    paste(x, collapse = ":")))))
-    ## 3.) Add interactions to full data frame
-    data_all_plus <- cbind(data_all_plus, df_interaction)
-    ## 4.) Drop interactions containing 80% or more empty cells
-    ## only for those with at least 80% of zeros this is operation relevant
-    colnames_interaction <- colnames(
-      df_interaction[, which(as.numeric(colSums(df_interaction == 0) / nrow(df_interaction)) >= 0.80)]
-    )
-    #colnames_interaction_sub <- colnames_interaction[1:10000]
-    gc()
-    i <- 0
-    for (colnames_interaction_sel in colnames_interaction) {
-      i <- i + 1
-      if (i %% 5000 == 0) {print(paste("Iteration", i))}
-      
-      colnames_interaction_sel_1 <- str_split(colnames_interaction_sel, ":")[[1]][1]
-      colnames_interaction_sel_2 <- str_split(colnames_interaction_sel, ":")[[1]][2]
-      num_rows_interaction <- 
-        data_all_plus %>% 
-        dplyr::select(all_of(colnames_interaction_sel), 
-                      all_of(colnames_interaction_sel_1), 
-                      all_of(colnames_interaction_sel_2)) %>%
-        filter(!!rlang::sym(colnames_interaction_sel_1) == 0 & 
-                 !!rlang::sym(colnames_interaction_sel_2) == 0) %>%
-        nrow()
-      num_rows_interaction <- num_rows_interaction / nrow(data_all_plus)
-      if (num_rows_interaction >= 0.80) {
-        data_all_plus <- data_all_plus %>% dplyr::select(-all_of(colnames_interaction_sel))
-      } else {
-        data_all_plus <- data_all_plus
-      }
-    }
-    # OLD: drop zeros
-    # df_interaction <- df_interaction[, which(as.numeric(colSums(df_interaction == 0) / nrow(df_interaction)) < 0.95)]
-    
-    #-- POLYNOMIALS --#
-    
-    ## Extract all numeric variables (from data frame without interactions)
-    cols_numeric_all <- names(unlist(lapply(data_binary, class)[lapply(data_binary, class) == "numeric"]))
-    
-    ## Drop columns that should not contain polynomials
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "outcome")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "treatment")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "na_")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "group")]
-    
-    ## Generate polynomials of degree 2 to 4
-    data_all_plus <- data_all_plus %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order2" = ~ .^2))) %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order3" = ~ .^3))) %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order4" = ~ .^4)))
     
     # save data frames
     if (cohort_prep == "controls_same_outcome") {
-      saveRDS(data_all_plus, 
-              paste0("Data/Grades/Prep_10/prep_10_dml_binary_allintpoly_", treatment_def, "_",
+      saveRDS(data_binary_all_plus, 
+              paste0("Data/Personality/Prep_10/prep_10_dml_binary_allintpoly_", treatment_def, "_",
                      treatment_repl, extra_act_save, "_mice", mice_data_sel, ".rds")
       )
     } else {
-      saveRDS(data_all_plus, 
-              paste0("Data/Grades/Prep_10/prep_10_dml_binary_allintpoly_", treatment_def, "_",
+      saveRDS(data_binary_all_plus, 
+              paste0("Data/Personality/Prep_10/prep_10_dml_binary_allintpoly_", treatment_def, "_",
                      treatment_repl, extra_act_save, "_robustcheck_mice", mice_data_sel, ".rds")
       )
     }
@@ -251,7 +269,9 @@ for (mice_data_sel in 1:5) {
     # again save mean after 5th iteration
     data_binary_num_cols_all <- rbind(
       data_binary_num_cols_all, 
-      data.frame("num_cols" = ncol(data_all_plus), "num_rows" = nrow(data_all_plus)))
+      data.frame("num_cols" = ncol(data_binary_all_plus), 
+                 "num_rows" = nrow(data_binary_all_plus))
+    )
     
     if (mice_data_sel == 5) {
       # SAVE NUMBER OF VARIABLES ETC.
@@ -263,18 +283,16 @@ for (mice_data_sel in 1:5) {
         "data_prep_treatment_def" = treatment_def,
         "data_prep_extraact" = extra_act,
         "num_id" = length(unique(data_final_raw$id_t)),
-        "num_rows" = mean(data_binary_num_cols_all$num_rows),
-        "num_cols" = mean(data_binary_num_cols_all$num_cols),
+        "num_rows" = round(mean(data_binary_num_cols_all$num_rows)),
+        "num_cols" = round(mean(data_binary_num_cols_all$num_cols)),
         "time_stamp" = Sys.time()
       )
       ## load function
       source("Functions/func_save_sample_reduction.R")
-      func_save_sample_reduction(df_excel_save, "grade")
+      func_save_sample_reduction(df_excel_save, "personality")
       gc()
     }
   }
-  
-  
   
   
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
@@ -382,78 +400,39 @@ for (mice_data_sel in 1:5) {
   if (create_interactions == "yes") {
     
     # create new data frame
-    data_multi_all_plus <- data_multi_all
+    drop_vars_multi <- c("treatment_sport", "treatment_sport_na",
+                         "treatment_sport_lag", "treatment_sport_lag_na", 
+                         "treatment_sport_source_uni", "treatment_sport_source_leisure",
+                         "treatment_sport_freq_never", "treatment_sport_freq_weekly_atleast",
+                         "treatment_sport_freq_never_lag", "treatment_sport_freq_weekly_atleast_lag")
+    drop_vars_multi <- drop_vars_multi[drop_vars_multi %in% colnames(data_all_plus)]
+    data_multi_all_plus <- data_all_plus %>% dplyr::select(-all_of(drop_vars_multi))
     
-    #-- INTERACTION TERMS --#
     
-    ## 1.) Drop treatment and outcome variables which are not used to generate interactions
-    df_interaction_multi <- data_multi_all_plus %>% 
-      dplyr::select(-c(starts_with("treatment"), starts_with("bigfive"), group))
-    ## 2.) Generate interactions
-    # https://stackoverflow.com/questions/31905221/r-generate-all-possible-interaction-variables
-    df_interaction_multi <- do.call(cbind, combn(colnames(df_interaction_multi), 2, FUN = function(x)
-      list(setNames(data.frame(df_interaction_multi[,x[1]]*df_interaction_multi[,x[2]]),
-                    paste(x, collapse = ":")))))
-    ## 3.) Add interactions to full data frame
-    data_multi_all_plus <- cbind(data_multi_all_plus, df_interaction_multi)
-    ## 4.) Drop interactions containing 80% or more empty cells
-    ## only for those with at least 80% of zeros this is operation relevant
-    colnames_interaction <- colnames(
-      df_interaction_multi[, which(as.numeric(colSums(df_interaction_multi == 0) / nrow(df_interaction_multi)) >= 0.80)]
-    )
-    #colnames_interaction_sub <- colnames_interaction[1:10000]
-    gc()
-    i <- 0
-    for (colnames_interaction_sel in colnames_interaction) {
-      i <- i + 1
-      if (i %% 5000 == 0) {print(paste("Iteration", i))}
-      
-      colnames_interaction_sel_1 <- str_split(colnames_interaction_sel, ":")[[1]][1]
-      colnames_interaction_sel_2 <- str_split(colnames_interaction_sel, ":")[[1]][2]
-      num_rows_interaction <- 
-        data_multi_all_plus %>% 
-        dplyr::select(all_of(colnames_interaction_sel), 
-                      all_of(colnames_interaction_sel_1), 
-                      all_of(colnames_interaction_sel_2)) %>%
-        filter(!!rlang::sym(colnames_interaction_sel_1) == 0 & 
-                 !!rlang::sym(colnames_interaction_sel_2) == 0) %>%
-        nrow()
-      num_rows_interaction <- num_rows_interaction / nrow(data_multi_all_plus)
-      if (num_rows_interaction >= 0.80) {
-        data_multi_all_plus <- data_multi_all_plus %>% dplyr::select(-all_of(colnames_interaction_sel))
-      } else {
-        data_multi_all_plus <- data_multi_all_plus
-      }
-    }
+    # create treatment_sport_freq dummy
+    data_multi_all_plus <- fastDummies::dummy_cols(
+      data_multi_all_plus, remove_selected_columns = FALSE, remove_first_dummy = FALSE, 
+      select_columns = c("treatment_sport_freq", "treatment_sport_freq_lag")
+    ) %>% dplyr::select(-c(treatment_sport_freq_lag, treatment_sport_freq_lag_never))
     
-    #-- POLYNOMIALS --#
-    
-    ## Extract all numeric variables (from data frame without interactions)
-    cols_numeric_all <- names(unlist(
-      lapply(data_multi_all, class)[lapply(data_multi_all, class) == "numeric"]
-    ))
-    
-    ## Drop columns that should not contain polynomials
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "outcome")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "treatment")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "na_")]
-    cols_numeric_all <- cols_numeric_all[!str_detect(cols_numeric_all, "group")]
-    
-    ## Generate polynomials of degree 2 to 4
+    # treatment_sport_freq as number
     data_multi_all_plus <- data_multi_all_plus %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order2" = ~ .^2))) %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order3" = ~ .^3))) %>%
-      mutate(across(all_of(cols_numeric_all), .fns = list("order4" = ~ .^4)))
+      mutate(treatment_sport_freq = case_when(
+        treatment_sport_freq == "weekly_atleast" ~ 1,
+        treatment_sport_freq == "monthly_less" ~ 2,
+        treatment_sport_freq == "never" ~ 3,
+        TRUE ~ 4
+      ))
     
     # save data frames
     if (cohort_prep == "controls_same_outcome") {
       saveRDS(data_multi_all_plus, 
-              paste0("Data/Grades/Prep_10/prep_10_dml_multi_allintpoly_", treatment_def, "_",
+              paste0("Data/Personality/Prep_10/prep_10_dml_multi_allintpoly_", treatment_def, "_",
                      treatment_repl, extra_act_save, "_mice", mice_data_sel, ".rds")
       )
     } else {
       saveRDS(data_multi_all_plus, 
-              paste0("Data/Grades/Prep_10/prep_10_dml_multi_allintpoly_", treatment_def, "_",
+              paste0("Data/Personality/Prep_10/prep_10_dml_multi_allintpoly_", treatment_def, "_",
                      treatment_repl, extra_act_save, "_robustcheck_mice", mice_data_sel, ".rds")
       )
     }
@@ -475,8 +454,8 @@ for (mice_data_sel in 1:5) {
         "data_prep_treatment_def" = treatment_def,
         "data_prep_extraact" = extra_act,
         "num_id" = length(unique(data_final_raw$id_t)),
-        "num_rows" = mean(data_multi_num_cols_all$num_rows),
-        "num_cols" = mean(data_multi_num_cols_all$num_cols),
+        "num_rows" = round(mean(data_multi_num_cols_all$num_rows)),
+        "num_cols" = round(mean(data_multi_num_cols_all$num_cols)),
         "time_stamp" = Sys.time()
       )
       ## load function
