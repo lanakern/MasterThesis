4301bcb79c3ae4634ebbcc11aae58486f8e2f470
Lana Kern <lana.kern@student.uni-tuebingen.de>
2023-04-28 10:19:43 +0200

    Remove descriptives (now in file 13)

--- Scripts/12_b_Assessment_Covariate_Balance.R
+++ Scripts/12_b_Assessment_Covariate_Balance.R
@@ -445,75 +445,7 @@ df_main_drivers <- df_smd_all_binary %>%
   head(50)
 
 
-# descriptive statistics for them using multivalued treatment setting
-data_all_mice_grades_multi <- data.frame()
-for (mice_data_sel in 1:5) {
-  data_load <- paste0("Data/Grades/Prep_10/prep_10_dml_multi_all_weekly_down_extradrop",
-                      cov_balance_save, "_mice", mice_data_sel, ".rds")
-  data_all_mice_sub <- readRDS(data_load)
-  data_all_mice_sub <- data_all_mice_sub %>% ungroup() %>% mutate(MICE = mice_data_sel)
-  data_all_mice_grades_multi <- rbind(data_all_mice_grades_multi, data_all_mice_sub)
-}
-cols_multi <- colnames(data_all_mice_grades_multi)[colnames(data_all_mice_grades_multi) %in% df_main_drivers$control_var]
-cols_multi <- c(cols_multi, "friends_study_share_(almost)half")
-
-df_descr <- data_all_mice_grades_multi %>% dplyr::select(treatment_sport_freq, all_of(cols_multi))
-
-df_descr <- left_join(
-  df_main_drivers, 
-  df_descr %>%
-    group_by(treatment_sport_freq) %>%
-    summarize_all(mean) %>%
-    gather(-treatment_sport_freq, key = "control_var", value = "mean") %>%
-    spread(key = treatment_sport_freq, value = mean) %>%
-    rename(mean_weekly = "1", mean_monthly = "2", mean_never = "3") %>%
-    mutate(control_var = case_when(
-      control_var == "friends_study_share_(almost)half" ~ "friends_study_share_.almost.half",
-      TRUE ~ control_var
-    )),
-  by = "control_var"
-)
-
-# add descriptives for students who are not physically active
-data_all_mice_grades_multi_noextra <- data.frame()
-for (mice_data_sel in 1:5) {
-  data_load <- paste0("Data/Grades/Prep_10/prep_10_dml_multi_all_weekly_down",
-                      cov_balance_save, "_mice", mice_data_sel, ".rds")
-  data_all_mice_sub <- readRDS(data_load)
-  data_all_mice_sub <- data_all_mice_sub %>% ungroup() %>% mutate(MICE = mice_data_sel)
-  data_all_mice_grades_multi_noextra <- rbind(data_all_mice_grades_multi_noextra, data_all_mice_sub)
-}
-data_all_mice_grades_multi_noextra <- data_all_mice_grades_multi_noextra %>%
-  filter(extracurricular_num == 0 & treatment_sport_freq == 3)
-cols_multi <- colnames(data_all_mice_grades_multi_noextra)[colnames(data_all_mice_grades_multi_noextra) %in% df_main_drivers$control_var]
-cols_multi <- c(cols_multi, "friends_study_share_(almost)half")
-
-df_descr_noextra <- data_all_mice_grades_multi_noextra %>% dplyr::select(treatment_sport_freq, all_of(cols_multi))
-
-df_descr_noextra <- left_join(
-  df_main_drivers, 
-  df_descr_noextra %>%
-    group_by(treatment_sport_freq) %>%
-    summarize_all(mean) %>%
-    gather(-treatment_sport_freq, key = "control_var", value = "mean") %>%
-    spread(key = treatment_sport_freq, value = mean) %>%
-    rename(mean_noextra = "3") %>%
-    mutate(control_var = case_when(
-      control_var == "friends_study_share_(almost)half" ~ "friends_study_share_.almost.half",
-      TRUE ~ control_var
-    )),
-  by = "control_var"
-)
-
-df_descr_all <- left_join(
-  df_descr %>% dplyr::select(-outcome), 
-  df_descr_noextra %>% dplyr::select(-c("outcome", "SD_before", "SD_after")), 
-  by = "control_var")
-
-# save
 saveRDS(df_main_drivers %>% as.data.frame(), "Output/DML/Covariate_Balancing/main_drivers.rds")
-saveRDS(df_descr_all %>% as.data.frame(), "Output/DML/Covariate_Balancing/main_drivers_descr.rds")
-
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
